2e4134a0bbf4624f71fa1a193b62a0343f9a1f68
douglaslima-pro <douglaslima-pro@outlook.com>
2024-05-07 17:12:52 -0300

    feat: adiciona a classe CPF e a feature validarCPF que utiliza a classe Pattern para comparar o valor inserido com o padrão esperado, baseando-se no uso de expressões regulares.

--- src/edu/bootcamp_java/exercicios/CPF.java
+++ src/edu/bootcamp_java/exercicios/CPF.java
@@ -1,27 +1,50 @@
 package edu.bootcamp_java.exercicios;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import edu.bootcamp_java.customExceptions.CPFInvalidoException;
 
 public class CPF {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite um CPF no formato XXX.XXX.XXX-XX: ");
+		/**
+		 * Número do CPF no formato XXX.XXX.XXX-XX
+		 */
+		String numeroDoCPF = sc.next();
+		/**
+		 * Armazena true se o CPF for válido, false o contrário
+		 */
 		boolean CPFValido = false;
 		try {
-			CPFValido = CPF.validarCPF("048.516.061-76");
+			CPFValido = CPF.validarCPF(numeroDoCPF);
 		} catch (CPFInvalidoException e) {
+			System.out.println();
 			System.out.println(e.getMessage());
+			System.out.println();
 		} finally {
+			System.out.println(numeroDoCPF);
 			System.out.println("CPF válido? " + (CPFValido ? "sim" : "não"));
 		}
 	}
 
+	/**
+	 * Verifica se o valor corresponde ao tipo {@code CPF} de acordo com o padrão esperado <strong>XXX.XXX.XXX-XX</strong>, sendo X um caractere do tipo numérico (0-9).
+	 * @param CPF
+	 * @return True se o valor corresponder ao padrão esperado, false o contrário.
+	 * @throws CPFInvalidoException
+	 */
 	public static boolean validarCPF(String CPF) throws CPFInvalidoException {
-		if (!Pattern.matches("\\d+", CPF)) {
-			throw new CPFInvalidoException("ERRO => CPF deve conter apenas números!");
+		if (CPF.length() != 14) {
+			throw new CPFInvalidoException("ERRO => O CPF deve conter exatamente 14 dígitos!");
 		}
-		if (CPF.length() != 11) {
-			throw new CPFInvalidoException("ERRO => CPF deve conter 11 dígitos!");
+		Pattern padraoCpf = Pattern.compile("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}");
+		Matcher matcherCpf = padraoCpf.matcher(CPF);
+		if (!matcherCpf.matches()) {
+			throw new CPFInvalidoException(
+					"ERRO => O CPF deve estar no formato XXX.XXX.XXX-XX, sendo X um caractere do tipo numérico (0-9).");
 		}
 		return true;
 	}
